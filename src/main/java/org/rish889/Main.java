ffe929bf2665abc2a8af2f4f37b75951e4bf5617
Rishabh Garcha <rish889@gmail.com>
2025-12-03 17:38:26 +0530

    rish Java

--- src/main/java/org/rish889/Main.java
+++ src/main/java/org/rish889/Main.java
@@ -1,18 +1,7 @@
 package org.rish889;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
-    public static void main(String[] args) {
+    static void main() {
         System.out.printf("Hello and welcome!");
-        List<String> rawList = new ArrayList<>();
-
-        List list = new ArrayList();  // raw type
-        List<String> strList = list;  // ⚠ unchecked assignment
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
     }
 }
