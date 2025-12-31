9c19d41c2c5325304e55d4b93d6f4d5ed6c5e43d
Rishabh Garcha <rish889@gmail.com>
2025-12-31 15:32:43 +0530

    rishCommit

--- src/main/java/org/rish889/Main.java
+++ src/main/java/org/rish889/Main.java
@@ -1,7 +1,20 @@
 package org.rish889;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Main {
     static void main() {
-        System.out.printf("Hello and welcome!");
+        List<String> words = List.of(
+                "java", "stream", "java", "collector",
+                "lambda", "stream", "java", "parallel"
+        );
+
+        Map<String, Long> wordFrequencyMap = words.stream()
+                .collect(Collectors.groupingBy(w -> w, LinkedHashMap::new, Collectors.counting()));
+        System.out.println(wordFrequencyMap);
+
     }
 }
--- src/main/java/org/rish889/virtual/threads/a0c0_virtual_threads_with_executor.java
+++ src/main/java/org/rish889/virtual/threads/a0c0_virtual_threads_with_executor.java
@@ -1,9 +1,16 @@
 package org.rish889.virtual.threads;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.*;
 
 public class a0c0_virtual_threads_with_executor {
     static void main() throws Exception {
+        createAVirtualThreadsWithExecutor();
+        createManyVirtualThreadsWithExecutor();
+    }
+
+    private static void createAVirtualThreadsWithExecutor() throws InterruptedException, ExecutionException {
         try (ExecutorService exec = Executors.newVirtualThreadPerTaskExecutor()) {
 
             Future<String> f = exec.submit(() -> {
@@ -13,5 +20,25 @@ public class a0c0_virtual_threads_with_executor {
 
             System.out.println(f.get());
         }
+    }
+
+    private static void createManyVirtualThreadsWithExecutor() {
+        ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
+        List<Future<String>> futureList = new ArrayList<>();
+        for (int i = 0; i < 1_000_000; i++) {
+            futureList.add(executor.submit(() -> {
+                Thread.sleep(1000);
+                return "Task done on " + Thread.currentThread();
+            }));
+        }
+        futureList.stream()
+                .forEach(future -> {
+                    try {
+                        System.out.println(future.get());
+                    } catch (Exception e) {
+                        System.out.println("Future failed");
+                    }
+                });
+        executor.shutdown();
     }
 }
