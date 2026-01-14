45afad48578ffd64c765e3cee4f68778b218b9c7
Rishabh Garcha <rish889@gmail.com>
2026-01-14 11:22:23 +0530

    rishCommit

---
+++ src/main/java/org/rish889/RomanConverter.java
@@ -0,0 +1,19 @@
+package org.rish889;
+
+public class RomanConverter {
+    private static final int[] VALUES = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private static final String[] SYMBOLS = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+    public String toRoman(int number) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < VALUES.length; i++) {
+            while (number >= VALUES[i]) {
+                result.append(SYMBOLS[i]);
+                number -= VALUES[i];
+            }
+        }
+
+        return result.toString();
+    }
+}
---
+++ src/test/java/org/rish889/RomanConverterTest.java
@@ -0,0 +1,31 @@
+package org.rish889;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RomanConverterTest {
+    private final RomanConverter converter = new RomanConverter();
+
+    @Test
+    public void testSimpleValues() {
+        assertEquals("I", converter.toRoman(1));
+        assertEquals("V", converter.toRoman(5));
+        assertEquals("X", converter.toRoman(10));
+    }
+
+    @Test
+    public void testAdditiveValues() {
+        assertEquals("II", converter.toRoman(2));
+        assertEquals("VI", converter.toRoman(6));
+        assertEquals("LVIII", converter.toRoman(58));
+    }
+
+    @Test
+    public void testSubtractiveValues() {
+        assertEquals("IV", converter.toRoman(4));
+        assertEquals("IX", converter.toRoman(9));
+        assertEquals("XL", converter.toRoman(40));
+        assertEquals("CM", converter.toRoman(900));
+    }
+}
