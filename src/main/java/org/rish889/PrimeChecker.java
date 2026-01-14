6fccc4dcba630e8f0dd38965ec1e1e8bcae26b58
Rishabh Garcha <rish889@gmail.com>
2026-01-14 11:07:56 +0530

    rishCommit

---
+++ src/main/java/org/rish889/PrimeChecker.java
@@ -0,0 +1,26 @@
+package org.rish889;
+
+public class PrimeChecker {
+
+    public boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+
+        if (number == 2) {
+            return true;
+        }
+
+        if (number % 2 == 0) {
+            return false;
+        }
+        
+        for (int i = 3; i <= Math.sqrt(number); i += 2) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ src/test/java/org/rish889/PrimeCheckerTest.java
@@ -0,0 +1,34 @@
+package org.rish889;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PrimeCheckerTest {
+    private final PrimeChecker checker = new PrimeChecker();
+
+    @Test
+    public void testPrimeNumbers() {
+        assertTrue(checker.isPrime(2), "2 should be prime");
+        assertTrue(checker.isPrime(3), "3 should be prime");
+        assertTrue(checker.isPrime(5), "5 should be prime");
+        assertTrue(checker.isPrime(13), "13 should be prime");
+        assertTrue(checker.isPrime(17), "17 should be prime");
+    }
+
+    @Test
+    public void testNonPrimeNumbers() {
+        assertFalse(checker.isPrime(4), "4 should not be prime");
+        assertFalse(checker.isPrime(6), "6 should not be prime");
+        assertFalse(checker.isPrime(9), "9 should not be prime");
+        assertFalse(checker.isPrime(15), "15 should not be prime");
+    }
+
+    @Test
+    public void testEdgeCases() {
+        assertFalse(checker.isPrime(1), "1 is not prime");
+        assertFalse(checker.isPrime(0), "0 is not prime");
+        assertFalse(checker.isPrime(-1), "-1 is not prime");
+        assertFalse(checker.isPrime(-10), "Negative numbers are not prime");
+    }
+}
