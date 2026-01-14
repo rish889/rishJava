c60567b32a5b38b9263139ffbf584eb59a80bec3
Rishabh Garcha <rish889@gmail.com>
2026-01-14 10:23:56 +0530

    rishCommit

---
+++ src/main/java/org/rish889/FizzBuzz.java
@@ -0,0 +1,15 @@
+package org.rish889;
+
+public class FizzBuzz {
+    public String fizzBuzz(int n) {
+        if (n % 3 == 0 && n % 5 == 0) {
+            return "FizzBuzz";
+        } else if (n % 3 == 0) {
+            return "Fizz";
+        } else if (n % 5 == 0) {
+            return "Buzz";
+        } else {
+            return String.valueOf(n);
+        }
+    }
+}
---
+++ src/test/java/org/rish889/FizzBuzzTest.java
@@ -0,0 +1,34 @@
+package org.rish889;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FizzBuzzTest {
+    private final FizzBuzz fb = new FizzBuzz();
+
+    @Test
+    public void testFizz() {
+        assertEquals("Fizz", fb.fizzBuzz(3));
+        assertEquals("Fizz", fb.fizzBuzz(6));
+    }
+
+    @Test
+    public void testBuzz() {
+        assertEquals("Buzz", fb.fizzBuzz(5));
+        assertEquals("Buzz", fb.fizzBuzz(10));
+    }
+
+    @Test
+    public void testFizzBuzz() {
+        assertEquals("FizzBuzz", fb.fizzBuzz(15));
+        assertEquals("FizzBuzz", fb.fizzBuzz(30));
+    }
+
+    @Test
+    public void testNumber() {
+        assertEquals("1", fb.fizzBuzz(1));
+        assertEquals("2", fb.fizzBuzz(2));
+        assertEquals("4", fb.fizzBuzz(4));
+        assertEquals("7", fb.fizzBuzz(7));
+    }
+}
