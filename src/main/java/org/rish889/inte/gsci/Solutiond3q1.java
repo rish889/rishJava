0f4c0723c8ee5fc9f14a0e8757e5e4b8ffed58ef
Rishabh Garcha <rish889@gmail.com>
2026-02-23 12:29:42 +0530

    rishCommit

--- src/main/java/org/rish889/inte/gsci/Solutiond3q1.java
+++ src/main/java/org/rish889/inte/gsci/Solutiond3q1.java
@@ -1,13 +1,37 @@
 package org.rish889.inte.gsci;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/*
+    Write a function to return the smallest number in a rotated sorted array.
+    Examples -
+    array = [3, 4, 5, 6, 1, 2], output = 1
+    array = [2, 1], output = 1
+ */
 public class Solutiond3q1 {
-    public static void main(String[] args) {
-        Solutiond3q1 sol = new Solutiond3q1();
-        System.out.println(sol.solution());
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return nums[left];
     }
 
-    public int solution() {
-        return -1;
+    @Test
+    void test1() {
+        assertEquals(1, findMin(new int[]{3, 4, 5, 6, 1, 2}));
+        assertEquals(1, findMin(new int[]{2, 1}));
     }
 }
 
