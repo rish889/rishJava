39688fb9c8c25d6ab834de341316233605f78e0f
Rishabh Garcha <rish889@gmail.com>
2026-02-23 12:16:35 +0530

    rishCommit

--- src/main/java/org/rish889/inte/gsci/Solutiond2q5.java
+++ src/main/java/org/rish889/inte/gsci/Solutiond2q5.java
@@ -1,5 +1,9 @@
 package org.rish889.inte.gsci;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /*
     For a string input the function returns output encoded as follows.
     input : a, output: a1
@@ -9,13 +13,34 @@ package org.rish889.inte.gsci;
     input : aabbbaa, output: a2b2a2
  */
 public class Solutiond2q5 {
-    public static void main(String[] args) {
-        Solutiond2q5 sol = new Solutiond2q5();
-        System.out.println(sol.solution());
+    public static String encode(String input) {
+        if (input == null || input.isEmpty()) {
+            return input;
+        }
+
+        StringBuilder result = new StringBuilder();
+        int count = 1;
+
+        for (int i = 1; i < input.length(); i++) {
+            if (input.charAt(i) == input.charAt(i - 1)) {
+                count++;
+            } else {
+                result.append(input.charAt(i - 1)).append(count);
+                count = 1;
+            }
+        }
+
+        result.append(input.charAt(input.length() - 1)).append(count);
+
+        return result.toString();
     }
 
-    public int solution() {
-        return -1;
+    @Test
+    void test1() {
+        assertEquals("a1", encode("a"));
+        assertEquals("a2", encode("aa"));
+        assertEquals("a2b3", encode("aabbb"));
+        assertEquals("a2b3a2", encode("aabbbaa"));
     }
 }
 
