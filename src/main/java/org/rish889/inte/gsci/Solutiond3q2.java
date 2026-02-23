3734be5ecec81bb49a3e8fea065762c2ae1316f7
Rishabh Garcha <rish889@gmail.com>
2026-02-23 12:35:06 +0530

    rishCommit

--- src/main/java/org/rish889/inte/gsci/Solutiond3q2.java
+++ src/main/java/org/rish889/inte/gsci/Solutiond3q2.java
@@ -1,19 +1,40 @@
 package org.rish889.inte.gsci;
 
-/*
-    Write a function to return second smallest element in array x. If x has fewer than 2 elements returns 0. Examples -
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/*
+    Write a function to return second smallest element in array x. If x has fewer than 2 elements returns 0.
+    Examples -
     x = [ -1, 0, 1, -2, 2], output = -1
     x = [ 0, 1], output = 1
  */
 public class Solutiond3q2 {
-    public static void main(String[] args) {
-        Solutiond3q2 sol = new Solutiond3q2();
-        System.out.println(sol.solution());
+    public static int secondSmallest(int[] x) {
+        if (x == null || x.length < 2) {
+            return 0;
+        }
+
+        int smallest = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+
+        for (int num : x) {
+            if (num < smallest) {
+                secondSmallest = smallest;
+                smallest = num;
+            } else if (num > smallest && num < secondSmallest) {
+                secondSmallest = num;
+            }
+        }
+
+        return secondSmallest == Integer.MAX_VALUE ? 0 : secondSmallest;
     }
 
-    public int solution() {
-        return -1;
+    @Test
+    void test1() {
+        assertEquals(-1, secondSmallest(new int[]{-1, 0, 1, -2, 2}));
+        assertEquals(1, secondSmallest(new int[]{0, 1}));
     }
 }
 
