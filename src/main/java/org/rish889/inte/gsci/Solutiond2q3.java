ad487ec6bf9f5e44fecd9a13da346594f5ef58b9
Rishabh Garcha <rish889@gmail.com>
2026-02-23 11:49:11 +0530

    rishCommit

--- src/main/java/org/rish889/inte/gsci/Solutiond2q3.java
+++ src/main/java/org/rish889/inte/gsci/Solutiond2q3.java
@@ -1,13 +1,32 @@
 package org.rish889.inte.gsci;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
+/*
+    Given a list of words, group them by anagrams.
+    Examples -
+        Input: ["cat", "dog", "god"], Output: [[“cat”], [“dog”,”god”]]
+        Input: ["pan", "nap", "rome", "more"], Output: [["pan", "nap"], ["rome", "more"]]
+ */
 public class Solutiond2q3 {
-    public static void main(String[] args) {
-        Solutiond2q3 sol = new Solutiond2q3();
-        System.out.println(sol.solution());
+    public static Collection<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+
+            map.computeIfAbsent(key, _ -> new ArrayList<>()).add(word);
+        }
+
+        return map.values();
     }
 
-    public int solution() {
-        return -1;
+    public static void main(String[] args) {
+        System.out.println(groupAnagrams(new String[]{"cat", "dog", "god"}));
+        System.out.println(groupAnagrams(new String[]{"pan", "nap", "rome", "more"}));
     }
 }
 
